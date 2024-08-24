5a180bdadb7e62f59256f1a69e3977623f2363d7
Allysson Carlos <allyssoncarlos.ac21@gmail.com>
2024-08-24 12:44:24 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿int n1;
+
+Console.WriteLine("Digite um numero: ");
+try
+{
+    n1 = int.Parse(Console.ReadLine());
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine(n1 + "x" + i + "=" + (n1*i));
+    }
+}
+catch (Exception digitou_letra)
+{
+    Console.WriteLine("Voce digitou uma letra! ");
+}
+finally
+{
+    Console.WriteLine("Pressione uma tecla para sair. ");
+    Console.ReadKey();
+}
